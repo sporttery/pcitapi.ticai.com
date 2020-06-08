fa628c5c6b2a7c73c4beef50c2433e4b9ed38476
zw <354190012@qq.com>
2020-06-08 19:04:52 +0800

    修改写入日志的内容

--- crontab.sh
+++ crontab.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+#chmod +x /etc/rc.d/rc.local
+#echo "nohup crontab.sh &" >> /etc/rc.d/rc.local
 logFile=/var/log/pcitapi.log
+datetime=$(date +"%F %T")
 while true
-curl -s http://pcitapi.ticai.com/api/getData 2>&1 >> $logFile
-curl -s http://pcitapi.ticai.com/api/getAwardResult 2>&1 >> $logFile
-sleep 1
+do
+    data=$(curl -s -m 2 http://pcitapi.ticai.com/api/getData 2>&1)
+    mcount=$(echo $data | wc -m)
+    if [ $mcount -gt 76 ] ; then
+        echo [$datetime] - $data >> $logFile
+    fi
+    result=$(curl -s -m 2 http://pcitapi.ticai.com/api/getAwardResult 2>&1)
+    mcount=$(echo $result | wc -m)
+    if [ $mcount -gt 60 ] ; then
+        echo [$datetime] - $result >> $logFile
+    fi
+    sleep 1.5
 done
